b37cd46d091f49983ab023a771ec3e98b1817b5d
Maya Mathur <mmathur@stanford.edu>
2023-04-07 06:45:26 -0700

    begin RSM_1 revision

--- prep_applied_MBMA.R
+++ prep_applied_MBMA.R
@@ -49,9 +49,9 @@ options(scipen=999)
 
 
 
-# EXAMPLE: AWR -----------------------------------------
+# EXAMPLE: MEAT CONSUMPTION -----------------------------------------
 
-# using this because has a clear randomized vs. nonrandomized indicator
+# using this meta-analysis because has a clear randomized vs. nonrandomized indicator
 setwd("~/Dropbox/Personal computer/Reference sheets/Library of prepped example meta-analyses/MRM")
 
 d = fread("mathur_awr_prepped.csv")
@@ -83,16 +83,8 @@ fwrite(d, "mathur_prepped.csv")
 
 
 
-# EXAMPLE (ROBUST): ATRIAL FIBRILLATION & COGNITIVE IMPAIRMENT -----------------------------------------
+# EXAMPLE: ATRIAL FIBRILLATION & COGNITIVE IMPAIRMENT -----------------------------------------
 
-# Kalantarian (saved PDF)
-# works great! worst-case estimate: log-RR 0.06, but CI could be shifted to null with eta = 7.51
-
-#**should compare our results to what they reported based on Egger's
-# paper talks a lot about confounding
-# could also analyze the subset of prospective studies; authors did this as well
-
-# ~ All patients regardless of stroke history -----------------------------------------
 
 # Figure 1
 # k = 14
@@ -168,67 +160,4 @@ fwrite(d, "kalantarian_all_prepped.csv")
 
 
 
-
-# ~ Only patients with stroke history -----------------------------------------
-
-
-# CAN'T ACTUALLY USE THIS BECAUSE ONLY 1 NONAFFIRMATIVE STUDY
-
-# # Figure 4
-# # k = 7
-# 
-# #**authors reported there WAS pub bias based on Egger's
-# RR = c(2.33,
-#        4.40,
-#        2.30,
-#        3.45,
-#        9.36,
-#        
-#        1.60,
-#        5.12)
-# 
-# hi = c(4.98,
-#        13.83,
-#        5.88,
-#        7.51,
-#        86.62,
-#        
-#        2.49,
-#        13.80)
-# 
-# 
-# study.type = c( rep("Prospective cohort", 5),
-#                 rep("Cross-sectional", 2) )
-# 
-# 
-# d = scrape_meta(est = RR,
-#                 hi = hi)
-# 
-# d = d %>% rename(vi = vyi)
-# d$sei = sqrt(d$vi)
-# d$pval = 2 * ( 1 - pnorm( abs( d$yi/sqrt(d$vi) ) ) )
-# d$affirm = d$yi > 0 & d$pval < 0.05
-# 
-# d$effect.measure = "log-rr"
-# 
-# # no apparent clustering from the forest plot
-# d$cluster = 1:nrow(d)
-# 
-# d$study.type = study.type
-# 
-# d = d %>% add_column(.before = 1,
-#                      meta.name = "kalantarian_stroke_hx")
-# 
-# # sanity check: reproduce reported analysis
-# # reported (abstract): RR: 2.70 [CI, 1.82 to 4.00]).
-# mod = rma.uni(yi = d$yi,
-#               vi = d$vi,
-#               method  = "DL")
-# expect_equal( as.numeric( round( exp(mod$b), 2 ) ), 2.70 )
-# expect_equal( as.numeric( round( exp(mod$ci.lb), 2 ) ), 1.82 )
-# # works :)
-# 
-# # save it
-# setwd(data.dir)
-# fwrite(d, "kalantarian_stroke_hx_prepped.csv")
 
